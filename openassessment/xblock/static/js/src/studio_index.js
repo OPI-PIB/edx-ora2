d04fd769ab236a4e9423811c4b34828eafab0fc3
Krystian Łygas <krystian.lygas@opi.org.pl>
2021-08-27 13:01:45 +0200

    Fix: add custom baseURL for tinymce

--- openassessment/xblock/static/js/src/studio_index.js
+++ openassessment/xblock/static/js/src/studio_index.js
@@ -2,4 +2,7 @@ import './oa_shared';
 
 import { OpenAssessmentEditor } from './studio/oa_edit';
 
+window.tinyMCE.baseURL = baseUrl + "navoica-theme/js/vendor/tinymce/js/tinymce";
+window.tinyMCE.suffix = ".min";
+
 window.OpenAssessmentEditor = OpenAssessmentEditor;
